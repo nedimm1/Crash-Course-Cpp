6b6345eebe84e25827b035c530057e226d1b9422
github_username <nedimmuminovic61@gmail.com>
2022-03-24 17:37:21 +0100

    new code for my program now with less hardcoding

--- Pproject1.cpp
+++ Pproject1.cpp
@@ -9,87 +9,6 @@ using namespace std;
 
 
 
-/*int main ()
-{    
-    char itemlist;
-    int price;
-    string anyelse;
-    char itemlist2;
-    cout << "welcome to myprogrammart.com" << endl;
-    cout << "what woud you like to buy?" << endl;
-    cout << "list of items:\nbag of chips 5$(C)\nbottle of Soda 7.99$(S)\nbread 9.99$ (B)\nNutela 15.99$ (N)\n" << endl;
-    
-    cin >> itemlist;
-
-    switch(itemlist) {
-    case 'C':
-        cout << "chips hase been added to the cart";
-        break;
-    case 'S':
-         cout << "soda hase been added to the cart";
-         break;
-    case 'B':
-         cout << "bread hase been added to the cart";
-         break;
-    case 'N':
-         cout << "Nutela hase been added to the cart";
-         break;
-    default:
-        cout << "we dont have that item, please pick a item from the list";
-         
-
-    }
-    
-     cout << "\nAnything else?" << endl;
-
-     cin >> anyelse;
-   
-    if(anyelse.at(0) == 'y' ) {//prees y to say yes
-
-        cin >> itemlist2;
-
-
-        switch(itemlist2) {
-    case 'C':
-        cout << "chips hase been added to the cart\n";
-        break;
-    case 'S':
-         cout << "soda hase been added to the cart\n";
-         break;
-    case 'B':
-         cout << "bread hase been added to the cart\n";
-         break;
-    case 'N':
-         cout << "Nutela hase been added to the cart\n";
-         break;
-    default:
-        cout << "we dont have that item, please pick a item from the list";
-
-         
-
-    }
-       
-      cout << "Now you have " << itemlist << " and " << itemlist2;
-    }
-
-   
-    else if(anyelse.at(0) == 'n') {//press n to say no 
-        cout << "cart: " << itemlist;
-    }
-    
-
-    
-    cout << "here is your recite: ";
-    
-    
- 
- 
-
- 
- 
-cout << endl;
- 
-}*/
 
 //shoping simulator3.0
 
@@ -97,9 +16,9 @@ cout << endl;
 
  
 int main(){
-
-    enum class Items{chips, soda, bread, nutela};
-    Items pick;
+   
+    char pick;
+    
     
 
     cout << "welcome to myprogrammart.com" << endl;
@@ -108,30 +27,17 @@ int main(){
     
     cin >> pick;
 
-    switch(pick){
-        case Items::chips:
-        cout << "chips hase been added to the cart ";
-        break;
-        case Items::soda:
-        cout << "soda has been added to the cart ";
-        break;
-        case Items::bread:
-        cout << "bread has been added to the cart";
-        break;
-        case Items::nutela:
-        cout << "nutela has been added to the cart";
-        break;
-        default:
-        cout << "we dont have that item, pick a item from the list please";
+   char options[] = {'c', 's', 'b', 'n'};
+string names[] = {"chips", "soda", "bread", "nutella"};
+
+cin >> pick;
+for(int i = 0; i < 4, i++;) {
+    if(pick == options[i]) {
+        cout << "You chose " << names[i] << endl;
     }
+}
 
-
-
-
-
-
-
-
+ cout << "you picked " << pick;
 
     cout << endl;
 
