3f37e8ca2d409dbd4118262b098e10455440dfda
github_username <nedimmuminovic61@gmail.com>
2022-03-28 21:49:40 +0200

    update: now i decided to use another methode now using maps in c++

--- Pproject1.cpp
+++ Pproject1.cpp
@@ -1,13 +1,17 @@
-//shoping simulator2.0
+
 #include <iostream> 
 #include <string>
+#include <map>
 #include <algorithm>
 #include <cmath>
 
 using namespace std;
 
 
-
+struct Item {
+    string name;
+    char symbol;
+};
 
 
 //shoping simulator3.0
@@ -19,25 +23,18 @@ int main(){
    
     char pick;
     
-    
+   
 
     cout << "welcome to myprogrammart.com" << endl;
     cout << "what woud you like to buy?" << endl;
     cout << "list of items:\nbag of chips 5$(C)\nbottle of Soda 7.99$(S)\nbread 9.99$ (B)\nNutela 15.99$ (N)\n" << endl;
     
-    cin >> pick;
+    
+     map<char, Item, double, Item> itemmap;
+  
+ 
 
-   char options[] = {'c', 's', 'b', 'n'};
-string names[] = {"chips", "soda", "bread", "nutella"};
 
-cin >> pick;
-for(int i = 0; i < 4, i++;) {
-    if(pick == options[i]) {
-        cout << "You chose " << names[i] << endl;
-    }
-}
-
- cout << "you picked " << pick;
 
     cout << endl;
 
